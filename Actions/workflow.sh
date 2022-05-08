080139c2af41d3d58ddbecf8a601c78aeb59926b
jmake <zvl_2006@hotmail.com>
2022-05-08 11:02:11 +0200

    Update workflow.sh

--- Actions/workflow.sh
+++ Actions/workflow.sh
@@ -1,3 +1,16 @@
+
+DOCKER_LOGIN() 
+{
+  docker login -u ${DUSERNAME} -p ${DPASSWORD}
+}
+
+
+DOCKER_PUSH() 
+{
+  docker push ${IMAGE_NAME}   
+}
+
+
 DOCKER_BUILD()
 {
   docker build . \
@@ -27,7 +40,13 @@ DOCKER_RUN()
 
 
 CONTAINER_NAME=nvcc_container
-IMAGE_NAME=nvcc_image 
+#IMAGE_NAME=nvcc_image 
+IMAGE_NAME=${DUSERNAME}/${DREPOSITORY}:$(date +%s)
+
+DUSERNAME
+DPASSWORD
+
+DOCKER_LOGIN
 
 DOCKER_BUILD 
 
@@ -39,6 +58,8 @@ DOCKER_RUN
 
 TOBEEXECUTED="bash RUNNER.sh"
 DOCKER_RUN
+
+DOCKER_PUSH
 
 #
 # docker run --rm --name nvcc_container --volume ${PWD}:/home --workdir /home -i nvcc_image bash GET_NVCC.sh
