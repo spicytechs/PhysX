64e7a8cf632bb2de0ba68be0435c1e1075af339b
jmake <zvl_2006@hotmail.com>
2022-05-08 11:36:50 +0200

    Update RUNNER.sh

--- Actions/RUNNER.sh
+++ Actions/RUNNER.sh
@@ -2,12 +2,32 @@
 INIT()
 {
   ## 1. SETUP 
-  g++ --version   ## g++ (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0
-  cmake --version ## cmake version 3.16.3
+  g++ --version       ## g++ (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0
+  cmake --version     ## cmake version 3.16.3
+  clang++-9 --version ## clang version 9.0.1-12 
 } 
 
 
 
 INIT
+
+ls
+pwd
+
+ROOT=${PWD}
+
+cd physx 
+ls
+bash generate_projects.sh linux 
+cd compiler/linux-release
+ls
+
+make -j install  
+
+ls
+
+cd $ROOT
+
+install/linux/PhysX/*
 
 ## 
